20a39c2ef03768e26dec58f562b87213de3c2d6d
林小蕾 <linxiaolei@xuetangx.com>
2019-04-04 19:15:17 +0800

    config:默认关闭elementpath,默认开启wordCount，实例配置maximumWords

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -82,7 +82,8 @@
               initialContent:'请输入题干内容...',   //初始化编辑器的内容,也可以通过textarea/script给值，看官网例子
               autoClearinitialContent:true, //是否自动清除编辑器初始内容，注意：如果focus属性设置为true,这个也为真，那么编辑器一上来就会触发导致初始化的内容看不到了
               initialFrameWidth: null,
-              initialFrameHeight: 250,
+							initialFrameHeight: 250,
+							maximumWords:10000,//允许的最大字符数
               BaseUrl: '',
               UEDITOR_HOME_URL: 'static/ueditor/'
 	        },
@@ -92,7 +93,8 @@
               initialContent:'请输入分析内容...',   //初始化编辑器的内容,也可以通过textarea/script给值，看官网例子
               autoClearinitialContent:true, //是否自动清除编辑器初始内容，注意：如果focus属性设置为true,这个也为真，那么编辑器一上来就会触发导致初始化的内容看不到了
               initialFrameWidth: null,
-              initialFrameHeight: 250,
+							initialFrameHeight: 250,
+							maximumWords:10000,//允许的最大字符数
               BaseUrl: '',
               UEDITOR_HOME_URL: 'static/ueditor/'
 	        },
@@ -102,7 +104,8 @@
               initialContent:'请输入解答内容...',   //初始化编辑器的内容,也可以通过textarea/script给值，看官网例子
               autoClearinitialContent:true, //是否自动清除编辑器初始内容，注意：如果focus属性设置为true,这个也为真，那么编辑器一上来就会触发导致初始化的内容看不到了
               initialFrameWidth: null,
-              initialFrameHeight: 250,
+							initialFrameHeight: 250,
+							maximumWords:10000,//允许的最大字符数
               BaseUrl: '',
               UEDITOR_HOME_URL: 'static/ueditor/'
 	        },
